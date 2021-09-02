fb6521052e85be0be715db51c3a4d9cd2155569b
+ <+>
2021-09-01 21:47:57 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+function superbowlWin(record) {
+   const win = record.find((date) => {
+        if (date.result === 'W') {
+            return date;
+        } 
+
+    });
+    
+    if (win === undefined) {
+        return win;
+    }
+    //console.log(win.year);
+    return win.year;
+
+
+}
+
