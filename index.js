f4a77c60276e62f766f59b831fa12a720c0a3e4c
+ <+>
2021-09-01 21:45:08 -0500

    commit tree 715c37ba2c71c79f1826f961db570837f4579524

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+function superbowlWin(record) {
+   const win = record.find((date) => {
+        if (date.result === 'W') {
+            console.log(`hi`);
+            return date;
+        } 
+
+    });
+    console.log(win);
+    if (win === undefined) {
+        return win;
+    }
+    //console.log(win.year);
+    return win.year;
+
+
+}
+
